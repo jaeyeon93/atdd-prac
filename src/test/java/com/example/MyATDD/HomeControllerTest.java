c57624b910572452ed60af1ffb4ab3a54cc30f5e
jaeyeon kim <foodsksms@gmail.com>
2018-05-25 17:41:45 +0900

    ATDD구현 동작

--- src/test/java/com/example/MyATDD/HomeControllerTest.java
+++ src/test/java/com/example/MyATDD/HomeControllerTest.java
@@ -1,24 +1,31 @@
 package com.example.MyATDD;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HomeControllerTest {
-    public static final Logger log =  LoggerFactory.getLogger(HomeControllerTest.class);
-    @Autowired
-    private TestRestTemplate template;
+    private static final Logger log = LoggerFactory.getLogger(HomeControllerTest.class);
+    @Autowired private TestRestTemplate template;
 
     @Test
     public void home() {
-        ResponseEntity<String> response = template.getForEntity("", String.class);
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
-        log.debug("body : {} ", response.getBody());
+        log.debug("body : {}", response.getBody());
     }
 }
